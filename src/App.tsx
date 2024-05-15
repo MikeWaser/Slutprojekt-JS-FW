a46c7811f0d09ba9478cbf859f5cbcfbe5159c20
MikeWaser <mik3waser@gmail.com>
2024-05-15 13:48:21 +0200

    wip

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./components/Styles/Scss/_reset.scss"
+import Navbar from "./components/Navbar/Navbar";
+
+
+function App() {
+  return (
+    <>
+      < Navbar />
+      <div>
+        <h1>Open libary</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
